19ac8cb7b5273f8947cce53021dde94e7683ffac
Arshiya Mohammad <mohammad.arshiya67@gmail.com>
2025-02-11 02:08:08 +0530

    Update add() to handle newlines between numbers

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,5 +1,5 @@
 class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers:
-            return sum(map(int, numbers.split(",")))
+            return sum(map(int, numbers.replace("\n", ",").split(",")))
         return 0
