4175ec65f8eed62c6ed349081334747eb2a1b8a7
Arshiya Mohammad <mohammad.arshiya67@gmail.com>
2025-02-11 02:43:53 +0530

    Update add() to handle multiple negative numbers

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -7,11 +7,10 @@ class StringCalculator:
             numbers = numbers[4:]
         else:
             delimiter = ","
-            
         splitted_numbers = list(map(int, numbers.replace("\n", ",").split(delimiter)))
+        negatives = [num for num in splitted_numbers if num < 0]
         
-        for num in splitted_numbers:
-            if num < 0:
-                raise ValueError(f"Negative numbers not allowed {num}")
+        if negatives:
+            raise ValueError(f"Negative numbers not allowed {', '.join(map(str, negatives))}")
         
         return sum(splitted_numbers)
