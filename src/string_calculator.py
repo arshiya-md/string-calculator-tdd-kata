1b2ec5719d253ea9da3caf021c98b483c1aef42a
Arshiya Mohammad <mohammad.arshiya67@gmail.com>
2025-02-11 02:18:39 +0530

    Update add() method to handle custom delimiters

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,5 +1,9 @@
 class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers:
+            if numbers.startswith("//"):
+                delimiter = numbers[2:3]
+                numbers = numbers[4:]
+                return sum(map(int, numbers.replace("\n", ",").split(delimiter)))
             return sum(map(int, numbers.replace("\n", ",").split(",")))
         return 0
