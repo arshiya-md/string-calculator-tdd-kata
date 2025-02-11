4371c76ec1777037d7430bfcccf27987195dc384
Arshiya Mohammad <mohammad.arshiya67@gmail.com>
2025-02-11 14:15:41 +0530

    Refactored code with test cases still passing

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -5,24 +5,31 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-       
+        
+        # If custom delimiters are provided, extract and process them
         if numbers.startswith("//"):
             delimiter_part, numbers = numbers.split("\n", 1)
             delimiters = re.findall(r"\[(.*?)\]", delimiter_part)
             if not delimiters:
-                delimiter = delimiter_part[2:]
-                numbers = numbers.replace(delimiter, ",")
-            else:
-                for delim in delimiters:
-                    numbers = numbers.replace(delim, ",")
+                delimiters = [delimiter_part[2:]]  # Extract the single delimiter
+            for delim in delimiters:
+                numbers = numbers.replace(delim, ",")
         else:
+            # Use a comma to split numbers when no custom delimiters are given
             numbers = numbers.replace("\n", ",")
             
-        splitted_numbers = list(map(int, numbers.split(',')))
-        negatives = [num for num in splitted_numbers if num < 0]
-        numbers_upto_1000 = [num for num in splitted_numbers if num <= 1000]
+        numbers_list = list(map(int, numbers.split(',')))
         
+        negatives = []
+        numbers_upto_1000 = []
+        
+        for num in numbers_list:
+            if num < 0:
+                negatives.append(num)
+            elif num <= 1000:
+                numbers_upto_1000.append(num)
+
         if negatives:
             raise ValueError(f"Negative numbers not allowed {', '.join(map(str, negatives))}")
-        
+
         return sum(numbers_upto_1000)
