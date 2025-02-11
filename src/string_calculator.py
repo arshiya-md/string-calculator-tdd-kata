a6e7cddc74ad6589d0b2050988e7eeb2ec9d3c4b
Arshiya Mohammad <mohammad.arshiya67@gmail.com>
2025-02-11 13:46:07 +0530

    Update add() to pass 5,6,7 test cases

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -5,15 +5,18 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-        
-        delimiters = ['\n', ',']
-        
+       
         if numbers.startswith("//"):
-            delimiter_part, numbers = numbers.split('\n',1)
+            delimiter_part, numbers = numbers.split("\n", 1)
             delimiters = re.findall(r"\[(.*?)\]", delimiter_part)
-            
-        for delim in delimiters:
-            numbers = numbers.replace(delim, ",")
+            if not delimiters:
+                delimiter = delimiter_part[2:]
+                numbers = numbers.replace(delimiter, ",")
+            else:
+                for delim in delimiters:
+                    numbers = numbers.replace(delim, ",")
+        else:
+            numbers = numbers.replace("\n", ",")
             
         splitted_numbers = list(map(int, numbers.split(',')))
         negatives = [num for num in splitted_numbers if num < 0]
