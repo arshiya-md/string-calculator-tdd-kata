645c2db8bdd48ae363738beb86eeda6c5f93e7ad
Arshiya Mohammad <mohammad.arshiya67@gmail.com>
2025-02-11 13:09:03 +0530

    Update add() to support multiple delimiters

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,13 +1,21 @@
+import re
+
+
 class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        
+        delimiters = ['\n', ',']
+        
         if numbers.startswith("//"):
-            delimiter = numbers[3:numbers.index(']')]
-            numbers = numbers[numbers.index('\n') + 1:]
-        else:
-            delimiter = ","
-        splitted_numbers = list(map(int, numbers.replace("\n", ",").split(delimiter)))
+            delimiter_part, numbers = numbers.split('\n',1)
+            delimiters = re.findall(r"\[(.*?)\]", delimiter_part)
+            
+        for delim in delimiters:
+            numbers = numbers.replace(delim, ",")
+            
+        splitted_numbers = list(map(int, numbers.split(',')))
         negatives = [num for num in splitted_numbers if num < 0]
         numbers_upto_1000 = [num for num in splitted_numbers if num <= 1000]
         
